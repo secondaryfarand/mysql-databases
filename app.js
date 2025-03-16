7ab1ab72bfe54e301dd94f265343f078c2800034
Second Farand <gambitmenteri8@gmail.com>
2025-03-16 21:50:11 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+import {db} from "./connection.js"
+// import db from "./connection.js"
+import {response} from "./response.js"
+
+
+
+
+app.get('/', (req, res) => {
+  const selectAllData = "SELECT * FROM mahasiswa" 
+  db.query(selectAllData, (err, result) => {
+    // hasil dari  my sql
+    if (err) {
+        console.error(err);
+        res.status(500).send('Terjadi kesalahan saat mengambil data');
+        return;
+      }
+      console.log(result)
+      response(200, result, "Get All Data", res)
+  })  
+})
+
+app.get('/find', (req, res) => {
+    const selectData = `SELECT nama_lengkap FROM mahasiswa WHERE nim  = ${req.query.nim}` 
+
+    console.log('Find NIM : ', req.query.nim)
+    db.query(selectData, (err, result) => {
+      // hasil dari  my sql
+      if (err) {
+          console.error(err);
+          res.status(500).send('Terjadi kesalahan saat mengambil data');
+          return;
+        }
+        console.log(result)
+        response(200, result, "Find Mahasiswa Name", res)
+    })  
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
